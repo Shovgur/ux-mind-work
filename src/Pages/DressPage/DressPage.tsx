06d23bc6196779ab0e5e242694c3582ad6efe7a8
Saveliy <jlays274@gmail.com>
2023-04-20 19:47:59 +0300

    fixed

--- src/Pages/DressPage/DressPage.tsx
+++ src/Pages/DressPage/DressPage.tsx
@@ -11,6 +11,7 @@ import {
   Radio,
   Text,
   Flex,
+  Image,
 } from "@chakra-ui/react";
 import { ChevronDownIcon } from "../../Assets/icons/ChevronIcon";
 import { Sort } from "../../Const";
@@ -88,52 +89,57 @@ const DressPage = ({ sort, setSort }: FilterProps) => {
           </Menu>
         </div>
       </div>
-      <div className={styles.dresspage__filter}>
-        <span className={styles.dresspage__filter_font}>Платья</span>
-        <div className={styles.dresspage__filter_category}>
-          {categoryButtons.map((value, index) => (
-            <Button
-              display="flex"
-              flexDirection="column"
-              alignItems="flex-start"
-              key={index}
-              h={34}
-              border="none"
-              cursor="pointer"
-              bgColor="white"
-              _hover={{ bg: "#ebedf0" }}
-              _active={{
-                bg: "#dddfe2",
-                transform: "scale(0.98)",
-              }}
-            >
-              {value.name}
-            </Button>
-          ))}
+      <div className={styles.dresspage__filter_card_container}>
+        <div className={styles.dresspage__filter}>
+          <span className={styles.dresspage__filter_font}>Платья</span>
+          <div className={styles.dresspage__filter_category}>
+            {categoryButtons.map((value, index) => (
+              <Button
+                display="flex"
+                flexDirection="column"
+                alignItems="flex-start"
+                key={index}
+                h={34}
+                border="none"
+                cursor="pointer"
+                bgColor="white"
+                _hover={{ bg: "#ebedf0" }}
+                _active={{
+                  bg: "#dddfe2",
+                  transform: "scale(0.98)",
+                }}
+              >
+                {value.name}
+              </Button>
+            ))}
+          </div>
+          <div className={styles.dresspage__filter_checkbox}>
+            <Text mt={20}> Размеры </Text>
+            <Stack gap={16}>{renderCheckboxList()}</Stack>
+          </div>
+          <div className={styles.dresspage__filter_radio}>
+            <Text mt={20}> Цвета </Text>
+            <Stack gap={18}>{renderRadioList()}</Stack>
+          </div>
+          <div className={styles.dresspage__filter_price}>
+            <Text>Цена</Text>
+            <Flex gap={10} alignItems="center" display="flex" direction="row">
+              <input
+                placeholder="от 9500"
+                className={styles.dresspage__filter_price_input}
+                type="text"
+              />
+              <div className={styles.dresspage__filter_price_line}></div>
+              <input
+                placeholder="до 46000"
+                className={styles.dresspage__filter_price_input}
+                type="text"
+              />
+            </Flex>
+          </div>
         </div>
-        <div className={styles.dresspage__filter_checkbox}>
-          <Text mt={20}> Размеры </Text>
-          <Stack gap={16}>{renderCheckboxList()}</Stack>
-        </div>
-        <div className={styles.dresspage__filter_radio}>
-          <Text mt={20}> Цвета </Text>
-          <Stack gap={18}>{renderRadioList()}</Stack>
-        </div>
-        <div className={styles.dresspage__filter_price}>
-          <Text>Цена</Text>
-          <Flex gap={10} alignItems="center" display="flex" direction="row">
-            <input
-              placeholder="от 9500"
-              className={styles.dresspage__filter_price_input}
-              type="text"
-            />
-            <div className={styles.dresspage__filter_price_line}></div>
-            <input
-              placeholder="до 46000"
-              className={styles.dresspage__filter_price_input}
-              type="text"
-            />
-          </Flex>
+        <div className={styles.dresspage__container_cardlist}>
+          <div className={styles.dresspage__container_card}><Image src="../../Assets/Image/DressImage/BlackDressImage.png"/></div>
         </div>
       </div>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -7,4 +7,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
